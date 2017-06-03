286cdbd60efe683bae0ce8ef41803dbfd780fd1b
Ioannis Pechlivanidis <i.pechlivanidis@gmail.com>
2017-06-03 13:41:29 +0300

    Initial commit.

---
+++ src/main/java/com/voxxedWorkshop/sumCalculator/CalculatorController.java
@@ -0,0 +1,13 @@
+package com.voxxedWorkshop.sumCalculator;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+    @RequestMapping("/sum")
+    String sum(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
+        return String.valueOf(a + b);
+    }
+}
---
+++ src/main/java/com/voxxedWorkshop/sumCalculator/SumCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.voxxedWorkshop.sumCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumCalculatorApplication.class, args);
+	}
+}
